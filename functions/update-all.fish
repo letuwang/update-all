a3c3408e2191de61155f3d15e005068617e37a5f
Letu Wang <wangletu57@gmail.com>
2023-05-19 17:44:31 -0700

    Apply the prompt for all packages

--- functions/update-all.fish
+++ functions/update-all.fish
@@ -3,18 +3,23 @@ function update-all
     sudo -v
 
     # fisher
+    _start_update_prompt "fisher"
     fisher update
 
     # homebrew
+    _start_update_prompt "homebrew"
     brew update && brew upgrade
 
     # pipx
+    _start_update_prompt "pipx"
     pipx upgrade-all
 
     # tlmgr
+    _start_update_prompt "tlmgr"
     sudo tlmgr update --self --all
 
     # conda
+    _start_update_prompt "conda"
     conda update --all
 end
 
